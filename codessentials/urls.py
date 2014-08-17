4e5040e80af87e0ec8ebed1bd1ea7f08990bc87d
Robin Schroer <sulamiification@gmail.com>
2014-08-17 16:18:15 +0200

    add posts url namespace

--- codessentials/urls.py
+++ codessentials/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'codessentials.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('posts.urls', namespace='posts')),
 )
+
