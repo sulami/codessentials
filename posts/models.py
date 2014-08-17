b62221a8489a218987c4ff1be49312f20ef4d539
Robin Schroer <sulamiification@gmail.com>
2014-08-17 14:07:00 +0200

    posts: add basic models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Language:
+    name = models.CharField(max_length=30)
+
+class Post:
+    name = models.CharField(max_length=140)
+    link = models.URLField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    language = models.ForeignKey(Language)
+
