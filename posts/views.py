d11b875251023563ad31daced5efe43ccfd58c7a
Robin Schroer <sulamiification@gmail.com>
2014-08-17 15:08:37 +0200

    add basic get function

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from posts import TextPost, VideoPost, BookPost
+
+"""
+Categories:
+t - text
+v - video
+b - book
+
+Modes:
+t - top
+n - new
+"""
+
+def index(request):
+    return redirect('get', 't', None, 't')
+
+def get(request, cat, language, mode):
+    if cat not in "tvb" or mode not in "tn":
+        return redirect('index')
+    if cat == "t":
+        posts = TextPost.objects.all()
+    if cat == "v":
+        posts = VideoPost.objects.all()
+    if cat == "b":
+        posts = BookPost.objects.all()
+    if language:
+        posts = posts.get(lang=language)
+    if mode == "t":
+        posts = posts.order_by('votes')
+    if mode == "n":
+        posts = posts.order_by('-pub_date')
+    context = {'posts': posts, 'cat': cat, 'lang': language, 'mode': mode}
+    return render('sometemplate.html', context)
+
+def post(request):
+    # TODO add posting functionalitu
+    pass
+
