74843d46a406947683720406a56e11f7e153f552
Robin Schroer <sulamiification@gmail.com>
2014-08-17 17:23:10 +0200

    models: add __str__()

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Language(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     """Base post class to inherit from"""
     name = models.CharField(max_length=140)
@@ -10,6 +13,9 @@ class Post(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     lang = models.ForeignKey('Language')
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
 
 class TextPost(Post):
     """Link->text post"""
