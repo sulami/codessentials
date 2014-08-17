87211ba6927560e921a3dd0157a462234f7a8599
Robin Schroer <sulamiification@gmail.com>
2014-08-17 14:23:12 +0200

    posts: add different post subclasses

--- posts/models.py
+++ posts/models.py
@@ -4,8 +4,21 @@ class Language:
     name = models.CharField(max_length=30)
 
 class Post:
+    """Base post class to inherit from"""
     name = models.CharField(max_length=140)
     link = models.URLField()
     pub_date = models.DateTimeField(auto_now_add=True)
     language = models.ForeignKey(Language)
+
+class LinkPost(Post):
+    """Link->text post"""
+    pass
+
+class VideoPost(Post):
+    """Link->video post"""
+    pass
+
+class BookPost(Post):
+    """Link->book post"""
+    pass
 
